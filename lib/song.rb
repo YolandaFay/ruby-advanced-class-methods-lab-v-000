560a287133fa8a0dab53d1cb58bc247470f98336
Yolanda Fay <yolandafay@gmail.com>
2018-11-03 04:27:47 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
   end
   
   def self.find_or_create_by_name(song_name)
-    test_song = self.all.find {|song| song.name = song_name} 
+    test_song = self.all.detect {|song| song.name = song_name} 
     if test_song == nil 
       song = self.new
       song.name = song_name 
