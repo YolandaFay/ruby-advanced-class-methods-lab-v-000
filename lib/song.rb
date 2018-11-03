85d9cf905f428fa5f16242908cebbc326ea77fe4
Yolanda Fay <yolandafay@gmail.com>
2018-11-03 04:14:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,9 +30,10 @@ class Song
   end 
   
   def self.find_by_name(song_name)
-    self.all.find {|song| song.name = song_name}  
-  
+    self.all.find {|song| song.name = song_name}
   end
+  
+  
   
 end
 
