7a8314af84e7eaaeb89aa43f42ecf1245ddb9cea
Yolanda Fay <yolandafay@gmail.com>
2018-11-03 04:34:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,8 +35,10 @@ class Song
   
   def self.find_or_create_by_name(song_name)
     
-    if find_by_name(song_name) != nil 
-      find_by_name(song_name)
+    test_song = find_by_name(song_name)
+    
+    if test_song != nil 
+      test_song 
     else
       create_by_name(song_name) 
     end
