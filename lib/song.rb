7387f590438a5cb3d3748478c9ac9c86f095f66c
Yolanda Fay <yolandafay@gmail.com>
2018-11-03 05:02:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,6 +50,7 @@ class Song
   
   def self.new_from_filename(filename_string)
     #filename_array = filename_string.split(/[.-]/)
+    filename_array = filename_string.split("- ")
     song = self.new 
     song.artist_name = filename_array[0]
     song.name = filename_array[1]
